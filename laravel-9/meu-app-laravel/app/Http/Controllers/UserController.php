5a44d62b8be40ec2c1a73c4ca4a9ae3ef31752ff
Douglas Alves <douglasreisalves02@gmail.com>
2022-06-27 13:11:12 -0300

    aulas iniciais de laravel

---
+++ laravel-9/meu-app-laravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = [
+            'nomes' => [
+                'José lira', 
+                'Jão lira'
+            ]
+        ];
+
+        dd($users);
+    }
+
+    public function show($id)
+    {
+        dd('Id do usuário é ' . $id);
+    }
+
+}
